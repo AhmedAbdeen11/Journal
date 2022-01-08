a8b3bb1f0cac3201e51fa272a7dba6abdf575af1
abdeen <ahmedabdeen11.freelancer@gmail.com>
2022-01-08 09:25:53 +0200

    Initial Commit

---
+++ Journal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Journal
+//
+//  Created by Abdeen on 1/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
